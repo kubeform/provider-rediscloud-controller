bea3a4c8c21c8bd2794613d1253d0bb09b11596b
1gtm <1gtm@appscode.com>
2021-07-30 15:19:43 +0000

    Generate code for provider@v0.2.5-0.20210730144149-20f14a5c8cdc gen@3dbd881
    Signed-off-by: 1gtm <1gtm@appscode.com>

---
+++ vendor/github.com/RedisLabs/rediscloud-go-api/CHANGELOG.md
@@ -0,0 +1,39 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+See updating [Changelog example here](https://keepachangelog.com/en/1.0.0/).
+
+## 0.1.4
+
+### Changed
+* Changed the UpdateDatabase struct to allow replicaOf to be set as empty array
+
+## 0.1.3
+
+### Changed
+* Expanded VPC Peering with fields for GCP and AWS
+
+## 0.1.2
+
+### Changed
+* Expanded VPC Peering with fields for GCP
+
+## 0.1.1
+
+### Added
+
+### Changed
+* Changed maximum number of retries when waiting for a task to finish so that it is compatible with 32bit environments.
+
+### Removed
+
+## 0.1.0
+
+### Added
+* List payment methods through the Accounts API 
+* Cloud accounts API
+* Subscription API
+* Database API
+
+### Changed
+
+### Removed
---
+++ vendor/github.com/RedisLabs/terraform-provider-rediscloud/rediscloud/provider/utils.go
@@ -0,0 +1,78 @@
+package provider
+
+import (
+	"fmt"
+	"github.com/RedisLabs/rediscloud-go-api/redis"
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"sync"
+)
+
+func validateDiagFunc(validateFunc func(interface{}, string) ([]string, []error)) schema.SchemaValidateDiagFunc {
+	return func(i interface{}, path cty.Path) diag.Diagnostics {
+		warnings, errs := validateFunc(i, fmt.Sprintf("%+v", path))
+		var diags diag.Diagnostics
+		for _, warning := range warnings {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Warning,
+				Summary:  warning,
+			})
+		}
+		for _, err := range errs {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  err.Error(),
+			})
+		}
+		return diags
+	}
+}
+
+func setToStringSlice(set *schema.Set) []*string {
+	var ret []*string
+	for _, s := range set.List() {
+		ret = append(ret, redis.String(s.(string)))
+	}
+	return ret
+}
+
+func interfaceToStringSlice(list []interface{}) []*string {
+	var ret []*string
+	for _, i := range list {
+		ret = append(ret, redis.String(i.(string)))
+	}
+	return ret
+}
+
+type perIdLock struct {
+	lock  sync.Mutex
+	store map[int]*sync.Mutex
+}
+
+func NewPerIdLock() *perIdLock {
+	return &perIdLock{
+		store: map[int]*sync.Mutex{},
+	}
+}
+
+func (m *perIdLock) Lock(id int) {
+	m.get(id).Lock()
+}
+
+func (m *perIdLock) Unlock(id int) {
+	m.get(id).Unlock()
+}
+
+func (m *perIdLock) get(id int) *sync.Mutex {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if v, ok := m.store[id]; ok {
+		return v
+	}
+
+	mutex := &sync.Mutex{}
+	m.store[id] = mutex
+	return mutex
+}
