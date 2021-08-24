39267d03dde4064e9638f5387827a4ac7cfdc320
1gtm <1gtm@appscode.com>
2021-08-24 08:01:29 +0000

    Generate code for provider@v0.2.5-0.20210730144149-20f14a5c8cdc gen@8c916bc
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- vendor/kubeform.dev/provider-rediscloud-api/apis/rediscloud/v1alpha1/subscription_webhook.go
+++ vendor/kubeform.dev/provider-rediscloud-api/apis/rediscloud/v1alpha1/subscription_webhook.go
@@ -20,9 +20,12 @@ package v1alpha1
 
 import (
 	"fmt"
+	"strings"
 
 	base "kubeform.dev/apimachinery/api/v1alpha1"
+	"kubeform.dev/provider-rediscloud-api/util"
 
+	jsoniter "github.com/json-iterator/go"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -38,6 +41,18 @@ func (r *Subscription) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 var _ webhook.Validator = &Subscription{}
 
+var subscriptionForceNewList = map[string]bool{
+	"/cloud_provider/*/cloud_account_id":                      true,
+	"/cloud_provider/*/provider":                              true,
+	"/cloud_provider/*/region/*/multiple_availability_zones":  true,
+	"/cloud_provider/*/region/*/networking_deployment_cidr":   true,
+	"/cloud_provider/*/region/*/networking_vpc_id":            true,
+	"/cloud_provider/*/region/*/preferred_availability_zones": true,
+	"/cloud_provider/*/region/*/region":                       true,
+	"/memory_storage":                                         true,
+	"/persistent_storage_encryption":                          true,
+}
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Subscription) ValidateCreate() error {
 	return nil
@@ -45,6 +60,53 @@ func (r *Subscription) ValidateCreate() error {
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Subscription) ValidateUpdate(old runtime.Object) error {
+	if r.Spec.Resource.ID == "" {
+		return nil
+	}
+	newObj := r.Spec.Resource
+	res := old.(*Subscription)
+	oldObj := res.Spec.Resource
+
+	jsnitr := jsoniter.Config{
+		EscapeHTML:             true,
+		SortMapKeys:            true,
+		TagKey:                 "tf",
+		ValidateJsonRawMessage: true,
+		TypeEncoders:           GetEncoder(),
+		TypeDecoders:           GetDecoder(),
+	}.Froze()
+
+	byteNew, err := jsnitr.Marshal(newObj)
+	if err != nil {
+		return err
+	}
+	tempNew := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteNew, &tempNew)
+	if err != nil {
+		return err
+	}
+
+	byteOld, err := jsnitr.Marshal(oldObj)
+	if err != nil {
+		return err
+	}
+	tempOld := make(map[string]interface{})
+	err = jsnitr.Unmarshal(byteOld, &tempOld)
+	if err != nil {
+		return err
+	}
+
+	for key := range subscriptionForceNewList {
+		keySplit := strings.Split(key, "/*")
+		length := len(keySplit)
+		checkIfAnyDif := false
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempOld, tempOld, tempNew)
+		util.CheckIfAnyDifference("", keySplit, 0, length, &checkIfAnyDif, tempNew, tempOld, tempNew)
+
+		if checkIfAnyDif && r.Spec.UpdatePolicy == base.UpdatePolicyDoNotDestroy {
+			return fmt.Errorf(`subscription "%v/%v" immutable field can't be updated. To update, change spec.updatePolicy to Destroy`, r.Namespace, r.Name)
+		}
+	}
 	return nil
 }
 
